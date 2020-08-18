834121cebf420834efa2e496da5ed2a26d7c69ab
miorgash <miorgash@gmail.com>
2020-08-18 12:05:19 +0000

    cut hard-coded path out of src

--- src/scripts/retrieval/document/batch_ir_ns.py
+++ src/scripts/retrieval/document/batch_ir_ns.py
@@ -15,16 +15,18 @@ def process(ranker, query, k=1):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', type=str, default=None)
-    parser.add_argument('--split',type=str)
     parser.add_argument('--count',type=int, default=1)
+    parser.add_argument('-i', '--in-file', type=str)
+    parser.add_argument('-o', '--out-file', type=str)
     args = parser.parse_args()
 
     k = args.count
-    split = args.split
     ranker = retriever.get_class('tfidf')(tfidf_path=args.model)
+    in_file = args.in_file
+    out_file = args.out_file
 
-    with open("data/fever-data/{0}.jsonl".format(split),"r") as f:
-        with open("data/fever/{0}.ns.pages.p{1}.jsonl".format(split,k),"w+") as f2:
+    with open(in_file, "r") as f:
+        with open(out_file, "w+") as f2:
             for line in tqdm(f.readlines()):
                 line = json.loads(line)
 
